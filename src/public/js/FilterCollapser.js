7b1ee4aaa054cbb01222b04323754e244ac40105
Adam Silver <adambsilver@gmail.com>
2017-12-27 13:02:20 +0000

    Filter collapser properly

---
+++ src/public/js/FilterCollapser.js
@@ -0,0 +1,32 @@
+function FilterCollapser(fieldset) {
+	this.fieldset = fieldset;
+	this.options = this.fieldset.find('.field-options');
+	this.legend = this.fieldset.find('legend');
+	this.createButton();
+	this.hide();
+}
+
+FilterCollapser.prototype.createButton = function() {
+	this.button = $('<button aria-expanded="true">'+this.legend.text()+'<svg viewBox="0 0 10 10" aria-hidden="true" focusable="false"><rect class="vert" height="8" width="2" y="1" x="4" /> <rect height="2" width="8" y="4" x="1" /></svg></button>');
+	this.button.on('click', $.proxy(this, 'onButtonClick'));
+	this.legend.html(this.button);
+};
+
+FilterCollapser.prototype.onButtonClick = function(e) {
+	e.preventDefault();
+	if(this.button.attr('aria-expanded') === 'true') {
+		this.hide();
+	} else {
+		this.show();
+	}
+};
+
+FilterCollapser.prototype.hide = function() {
+	this.button.attr('aria-expanded', 'false');
+	this.options.addClass('hidden');
+};
+
+FilterCollapser.prototype.show = function() {
+	this.button.attr('aria-expanded', 'true');
+	this.options.removeClass('hidden');
+};
