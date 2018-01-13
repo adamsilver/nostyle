329bc94a3aacf19596877c3395314a9ae599fce4
Adam Silver <adambsilver@gmail.com>
2018-01-13 20:33:20 +0000

    Updated add another

--- src/public/js/AddAnotherForm.js
+++ src/public/js/AddAnotherForm.js
@@ -2,7 +2,6 @@ function AddAnotherForm(container) {
 	this.container = $(container);
 	this.container.on('click', '.addAnother-removeButton', $.proxy(this, 'onRemoveButtonClick'));
 	this.container.on('click', '.addAnother-addButton', $.proxy(this, 'onAddButtonClick'));
-	this.setupRemoveButtons();
 	this.createStatusBox();
 }
 
@@ -11,70 +10,51 @@ AddAnotherForm.prototype.createStatusBox = function() {
 	this.container.append(this.status);
 };
 
-AddAnotherForm.prototype.setupRemoveButtons = function(e) {
-	var items = this.container.find('.addAnother-item');
-	items.each(function(i, el) {
-		if(i != 0) {
-			$(el).append('<button class="addAnother-removeButton" type="button">- Remove</button>');
-		}
-	});
-};
-
 AddAnotherForm.prototype.onAddButtonClick = function(e) {
 	e.preventDefault();
-	this.cloneItem();
-	this.checkFirstItemRemoveButton();
+	var item = this.getNewItem();
+	this.updateAttributes(this.getItems().length, item);
+	// this.resetItem(item);
+	var firstItem = this.getItems().first();
+	if(!this.hasRemoveButton(firstItem)) {
+		this.createRemoveButton(firstItem);	
+	}
+	this.getItems().last().after(item);
+	// this.focusField(item);
 };
 
-AddAnotherForm.prototype.cloneItem = function(items) {
-	var items = this.container.find('.addAnother-item');
-	var newItem = items.first().clone();
-	this.appendRemoveButton(newItem);
-	this.updateItemAttributes(newItem, items.length);
-	this.resetItem(newItem);
-	items.last().after(newItem);
-	this.focusField(newItem);
+AddAnotherForm.prototype.hasRemoveButton = function(item) {
+	return item.find('.addAnother-removeButton').length;
 };
 
-AddAnotherForm.prototype.updateItemAttributes = function(item, index) {
-	item.find('[data-name], [data-id]').each(function(i, el) {
+AddAnotherForm.prototype.getItems = function() {
+	return this.container.find('.addAnother-item');
+};
+
+AddAnotherForm.prototype.getNewItem = function() {
+	var item = this.getItems().first().clone();
+	if(!this.hasRemoveButton(item)) {
+		this.createRemoveButton(item);
+	}
+	return item;
+};
+
+AddAnotherForm.prototype.updateAttributes = function(index, item) {
+	item.find('[data-name]').each(function(i, el) {
 		el.name = $(el).attr('data-name').replace(/%index%/, index);
 		el.id = $(el).attr('data-id').replace(/%index%/, index);
-		var label = $(el).parents('label');
-		if(label.length) {
-			label[0].htmlFor = el.id;
-		} else {
-			label = $(el).prev('label');
-			label[0].htmlFor = el.id;
-		}
+		($(el).prev('label')[0] || $(el).parents('label')[0]).htmlFor = el.id;
 	});
 };
 
-AddAnotherForm.prototype.checkFirstItemRemoveButton = function() {
-	var items = this.container.find('.addAnother-item');
-	if(items.length === 2) {
-		this.appendRemoveButton(items.first());
-	}
-};
-
-AddAnotherForm.prototype.appendRemoveButton = function(item) {
+AddAnotherForm.prototype.createRemoveButton = function(item) {
 	item.append('<button type="button" class="secondaryButton addAnother-removeButton">Remove</button>');
 };
 
-AddAnotherForm.prototype.updateAttributes = function() {
-	this.container.find('.addAnother-item').each(function(index, el) {
-		$(el).find('[data-name], [data-id]').each(function(index2, el) {
-			el.name = $(el).attr('data-name').replace(/%index%/, index);
-			el.id = $(el).attr('data-id').replace(/%index%/, index);
-			var label = $(el).parents('label');
-			if(label.length) {
-				label[0].htmlFor = el.id;
-			} else {
-				label = $(el).prev('label');
-				label[0].htmlFor = el.id;
-			}
-		});
-	});
+AddAnotherForm.prototype.updateAllAttributes = function() {
+	this.container.find('.addAnother-item').each($.proxy(function(index, el) {
+		this.updateAttributes(index, $(el));
+	}, this));
 };
 
 AddAnotherForm.prototype.resetItem = function(item) {
@@ -93,12 +73,12 @@ AddAnotherForm.prototype.focusField = function(item) {
 
 AddAnotherForm.prototype.onRemoveButtonClick = function(e) {
 	e.preventDefault();
-	var items = this.container.find('.addAnother-item');
+	var items = this.getItems();
 	$(e.currentTarget).parents('.addAnother-item').remove();
 	if(items.length == 2) {
 		items.find('.addAnother-removeButton').remove();
 	}
-	this.updateAttributes();
+	this.updateAllAttributes();
 	this.focusHeading();
 };
 
