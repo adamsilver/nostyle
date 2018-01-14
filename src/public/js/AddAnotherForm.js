ba4cd9c4ffbbb632d5a223056c1163f1a6773963
Adam Silver <adambsilver@gmail.com>
2018-01-14 07:22:16 +0000

    Updated

--- src/public/js/AddAnotherForm.js
+++ src/public/js/AddAnotherForm.js
@@ -2,25 +2,19 @@ function AddAnotherForm(container) {
 	this.container = $(container);
 	this.container.on('click', '.addAnother-removeButton', $.proxy(this, 'onRemoveButtonClick'));
 	this.container.on('click', '.addAnother-addButton', $.proxy(this, 'onAddButtonClick'));
-	this.createStatusBox();
+	this.container.find('.addAnother-addButton').prop('type', 'button');
 }
 
-AddAnotherForm.prototype.createStatusBox = function() {
-	this.status = $('<div role="status" aria-live="polite"></div>');
-	this.container.append(this.status);
-};
-
 AddAnotherForm.prototype.onAddButtonClick = function(e) {
-	e.preventDefault();
 	var item = this.getNewItem();
 	this.updateAttributes(this.getItems().length, item);
-	// this.resetItem(item);
+	this.resetItem(item);
 	var firstItem = this.getItems().first();
 	if(!this.hasRemoveButton(firstItem)) {
 		this.createRemoveButton(firstItem);	
 	}
 	this.getItems().last().after(item);
-	// this.focusField(item);
+	item.find('.field').first().find('input, textarea, select').focus();
 };
 
 AddAnotherForm.prototype.hasRemoveButton = function(item) {
@@ -67,14 +61,10 @@ AddAnotherForm.prototype.resetItem = function(item) {
 	});
 };
 
-AddAnotherForm.prototype.focusField = function(item) {
-	item.find('.field').first().find('input, textarea, select').focus();
-};
-
 AddAnotherForm.prototype.onRemoveButtonClick = function(e) {
 	e.preventDefault();
-	var items = this.getItems();
 	$(e.currentTarget).parents('.addAnother-item').remove();
+	var items = this.getItems();
 	if(items.length == 2) {
 		items.find('.addAnother-removeButton').remove();
 	}
