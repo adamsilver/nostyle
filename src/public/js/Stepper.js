bc75ab329940b7162a051749c46d5eb682bcb6c2
Adam Silver <adambsilver@gmail.com>
2018-02-26 08:07:14 +0000

    Udpated

--- src/public/js/Stepper.js
+++ src/public/js/Stepper.js
@@ -5,11 +5,11 @@ function Stepper(input, options) {
 	this.wrapper = $('<div class="stepper"></div>');
 	this.wrapper.append(this.input);
 	this.container.append(this.wrapper);
-	this.createDecrementButton();
-	this.createIncrementButton();
+	this.createRemoveButton();
+	this.createAddButton();
 	this.createStatusBox();
-	this.wrapper.on('click', '.stepper-decrementButton', $.proxy(this, 'onDecrementClick'));
-	this.wrapper.on('click', '.stepper-incrementButton', $.proxy(this, 'onIncrementClick'));
+	this.wrapper.on('click', '.stepper-removeButton', $.proxy(this, 'onRemoveClick'));
+	this.wrapper.on('click', '.stepper-addButton', $.proxy(this, 'onAddClick'));
 }
 
 Stepper.prototype.createStatusBox = function() {
@@ -19,26 +19,26 @@ Stepper.prototype.createStatusBox = function() {
 
 Stepper.prototype.setOptions = function(options) {
 	var defaults = {
-		decrementLabel: 'Decrement',
-		incrementLabel: 'Increment'
+		decrementLabel: 'Remove',
+		incrementLabel: 'Add'
 	};
 	this.options = $.extend(defaults, options);
 };
 
-Stepper.prototype.createDecrementButton = function() {
-	this.decrementButton = $('<button class="stepper-decrementButton" type="button" aria-label="'+this.options.decrementLabel+'">&minus;</button>');
+Stepper.prototype.createRemoveButton = function() {
+	this.removeButton = $('<button class="stepper-removeButton" type="button" aria-label="'+this.options.removeLabel+'">&minus;</button>');
 	if(this.options.labelId) {
-		this.decrementButton.attr('aria-describedby', this.options.labelId);
+		this.removeButton.attr('aria-describedby', this.options.labelId);
 	}
-	this.wrapper.prepend(this.decrementButton);
+	this.wrapper.prepend(this.removeButton);
 };
 
-Stepper.prototype.createIncrementButton = function() {
-	this.incrementButton = $('<button class="stepper-incrementButton" type="button" aria-label="'+this.options.incrementLabel+'">&#43;</button>');
+Stepper.prototype.createAddButton = function() {
+	this.addButton = $('<button class="stepper-addButton" type="button" aria-label="'+this.options.addLabel+'">&#43;</button>');
 	if(this.options.labelId) {
-		this.incrementButton.attr('aria-describedby', this.options.labelId);
+		this.addButton.attr('aria-describedby', this.options.labelId);
 	}
-	this.wrapper.append(this.incrementButton);
+	this.wrapper.append(this.addButton);
 };
 
 Stepper.prototype.getInputValue = function() {
@@ -49,7 +49,7 @@ Stepper.prototype.getInputValue = function() {
 	return val;
 };
 
-Stepper.prototype.onDecrementClick = function(e) {
+Stepper.prototype.onRemoveClick = function(e) {
 	var newVal = this.getInputValue() - 1;
 	if(newVal >= parseInt(this.input.attr('min'), 10)) {
 		this.input.val(newVal);
@@ -57,7 +57,7 @@ Stepper.prototype.onDecrementClick = function(e) {
 	}
 };
 
-Stepper.prototype.onIncrementClick = function(e) {
+Stepper.prototype.onAddClick = function(e) {
 	var newVal = this.getInputValue() + 1;
 	if(newVal <= parseInt(this.input.attr('max'), 10)) {
 		this.input.val(newVal);
