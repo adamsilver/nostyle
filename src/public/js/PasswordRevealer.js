e5981703d4ddeceb3ccf529b37df204f872179de
Adam Silver <adambsilver@gmail.com>
2018-03-21 15:32:03 +0000

    Improve password reveal

--- src/public/js/PasswordRevealer.js
+++ src/public/js/PasswordRevealer.js
@@ -1,12 +1,12 @@
 function PasswordRevealer(el) {
   this.el = el;
+  $(this.el).wrap('<div class="passwordRevealer"></div>');
   this.container = $(this.el).parent();
-  this.container.addClass('passwordRevealer');
   this.createButton();
 };
 
 PasswordRevealer.prototype.createButton = function() {
-  this.button = $('<button type="button" class="secondaryButton">Show</button>');
+  this.button = $('<button type="button">Show</button>');
   this.container.append(this.button);
   this.button.on('click', $.proxy(this, 'onButtonClick'));
 };
