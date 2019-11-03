811e34e9a88e5a838c9742fc42cb5fe6592af773
Adam Silver <adambsilver@gmail.com>
2019-11-03 07:33:28 +0000

    Inter and filters

--- src/app/data/products.js
+++ src/app/data/products.js
@@ -1,72 +1,72 @@
 module.exports = {
 	products: [{
 		color: 'red',
-		hex: '#FF4136',
+		hex: '#F56565',
 		rating: 2,
 		id: 1
 	}, {
-		color: 'blue',
-		hex: '#0074D9',
+		color: 'yellow',
+		hex: '#ECC94B',
 		rating: 1,
 		id: 2
 	}, {
 		color: 'green',
-		hex: '#2ECC40',
+		hex: '#48BB78',
 		rating: 1,
 		id: 3
 	}, {
 		color: 'red',
-		hex: '#FF4136',
+		hex: '#F56565',
 		rating: 1,
 		id: 4
 	}, {
-		color: 'blue',
-		hex: '#0074D9',
+		color: 'yellow',
+		hex: '#ECC94B',
 		rating: 1,
 		id: 5
 	}, {
 		color: 'green',
-		hex: '#2ECC40',
+		hex: '#48BB78',
 		rating: 2,
 		id: 6
 	}, {
 		color: 'red',
-		hex: '#FF4136',
+		hex: '#F56565',
 		rating: 1,
 		id: 7
 	}, {
-		color: 'blue',
-		hex: '#0074D9',
+		color: 'yellow',
+		hex: '#ECC94B',
 		rating: 1,
 		id: 8
 	}, {
 		color: 'green',
-		hex: '#2ECC40',
+		hex: '#48BB78',
 		rating: 2,
 		id: 9
 	}, {
 		color: 'red',
-		hex: '#FF4136',
+		hex: '#F56565',
 		rating: 1,
 		id: 10
 	}, {
-		color: 'blue',
-		hex: '#0074D9',
+		color: 'yellow',
+		hex: '#ECC94B',
 		rating: 2,
 		id: 11
 	}, {
 		color: 'green',
-		hex: '#2ECC40',
+		hex: '#48BB78',
 		rating: 3,
 		id: 12
 	}, {
 		color: 'red',
-		hex: '#FF4136',
+		hex: '#F56565',
 		rating: 4,
 		id: 13
 	}, {
 		color: 'green',
-		hex: '#2ECC40',
+		hex: '#48BB78',
 		rating: 3,
 		id: 14
 	}]
